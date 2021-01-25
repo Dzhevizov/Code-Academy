bbc7623af0b81a83c583005bf195080f80f061ea
Hristo Dzhevizov <76977957+Dzhevizov@users.noreply.github.com>
2021-01-25 15:16:17 +0200

    Create 20210125_3.c

---
+++ 20210125/20210125_3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void)
+{
+    char c;
+    c = getchar();
+    int nl = 0;
+    int nt = 0;
+    int ns = 0;
+    while (c != EOF)
+    {
+        c = getchar();
+        if (c == '\n'){
+            nl++;
+        }
+        if (c == '\t'){
+            nt++;
+        }
+        if (c == ' '){
+            ns++;
+        }
+        
+    }
+    printf("New lines: %d\n", nl);
+    printf("Tabs: %d\n", nt);
+    printf("Spaces: %d\n", ns);
+    return 0;
+}
