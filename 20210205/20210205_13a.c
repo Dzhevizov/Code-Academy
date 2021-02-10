f13dd3f61b60cedb2e26465da0ef8f817f323a9f
Hristo Dzhevizov <76977957+Dzhevizov@users.noreply.github.com>
2021-02-10 02:32:38 +0200

    Create 20210205_13a.c

---
+++ 20210205/20210205_13a.c
@@ -0,0 +1,18 @@
+/*Дефинирайте и инициализирайте едномерен масив с 5
+елемента. Следкато сте готови, направете въвеждане на данните в масива,
+като четете от потребителя със scanf.*/
+
+#include <stdio.h>
+
+int main(){
+    
+    int array [5] = {0};
+    int i;
+    printf("Enter 5 elements of an array\n");
+    for (i = 0; i < 5; i++){
+        scanf("%d", &array[i]);
+    }
+    printf("Your array is: {%d,%d,%d,%d,%d}\n", array[0], array[1], array[2], array[3], array[4]);
+    
+    return 0;
+}
