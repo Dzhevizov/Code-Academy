b737a2c09c17255d851c548f66004dc12745a627
Hristo Dzhevizov <76977957+Dzhevizov@users.noreply.github.com>
2021-03-07 11:16:48 +0200

    Update 20210305_4.c

--- 20210305/20210305_4.c
+++ 20210305/20210305_4.c
@@ -16,27 +16,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct player
-{
+#define ROW 7
+#define COL 7
+
+int playground[ROW][COL] = {
+    3, 3, 2, 4, 3, 1, 2,
+    2, 4, 2, 3, 2, 4, 3,
+    4, 2, 3, 2, 4, 2, 1,
+    4, 4, 1, 2, 2, 3, 4,
+    3, 2, 3, 3, 4, 2, 2,
+    3, 2, 4, 2, 3, 2, 1,
+    1, 1, 3, 3, 4, 2, 22
+};
+
+struct player{
     int x;
     int y;
 };
 
-int main(){
-    int playground[7][7] = {
-        3, 3, 2, 4, 3, 1, 2,
-        2, 4, 2, 3, 2, 4, 3,
-        4, 2, 3, 2, 4, 2, 1,
-        4, 4, 1, 2, 2, 3, 4,
-        3, 2, 3, 3, 4, 2, 2,
-        3, 2, 4, 2, 3, 2, 1,
-        1, 1, 3, 3, 4, 2, 22
-    };
+void playerMove(int *ptr, struct player player1, struct player player2);
 
+int main(void){ 
     int *ptr = (int*)playground;
 
-    for (int i = 0; i < 7; i++){
-        for (int j = 0; j < 7; j++){
+    for (int i = 0; i < ROW; i++){
+        for (int j = 0; j < COL; j++){
             printf("%c ", *(ptr + 7 * i + j) + 48);
         }
         printf("\n");
@@ -44,8 +48,13 @@ int main(){
 
     struct player player1 = {0,0};
     struct player player2 = {0,0};
-    
-    int direction;
+    playerMove(ptr, player1, player2);
+    free(ptr);
+    return 0;   
+}
+
+void playerMove(int *ptr, struct player player1, struct player player2){
+        int direction;
 
     player1_move:
     printf("player1 choose direction\n");
@@ -54,8 +63,8 @@ int main(){
 
     switch (direction){
         case 8:
-            if ((player1.x - *(ptr + 7 * player1.x + player1.y)) >= 0){
-                player1.x = player1.x - *(ptr + 7 * player1.x + player1.y);
+            if ((player1.x - *(ptr + ROW * player1.x + player1.y)) >= 0){
+                player1.x = player1.x - *(ptr + ROW * player1.x + player1.y);
             }
             else {
                 printf("Invalid direction\n");
@@ -63,8 +72,8 @@ int main(){
             }
             break;
         case 2:
-            if ((player1.x + *(ptr + 7 * player1.x + player1.y)) < 7){
-                player1.x = player1.x + *(ptr + 7 * player1.x + player1.y);
+            if ((player1.x + *(ptr + ROW * player1.x + player1.y)) < ROW){
+                player1.x = player1.x + *(ptr + ROW * player1.x + player1.y);
             }
             else {
                 printf("Invalid direction\n");
@@ -72,8 +81,8 @@ int main(){
             }
             break;
         case 4:
-            if ((player1.y - *(ptr + 7 * player1.x + player1.y)) >= 0){
-                player1.y = player1.y - *(ptr + 7 * player1.x + player1.y);
+            if ((player1.y - *(ptr + COL * player1.x + player1.y)) >= 0){
+                player1.y = player1.y - *(ptr + COL * player1.x + player1.y);
             }
             else {
                 printf("Invalid direction\n");
@@ -81,8 +90,8 @@ int main(){
             }
             break;
         case 6:
-            if ((player1.y + *(ptr + 7 * player1.x + player1.y)) < 7){
-                player1.y = player1.y + *(ptr + 7 * player1.x + player1.y);
+            if ((player1.y + *(ptr + COL * player1.x + player1.y)) < COL){
+                player1.y = player1.y + *(ptr + COL * player1.x + player1.y);
             }
             else {
                 printf("Invalid direction\n");
@@ -99,7 +108,7 @@ int main(){
 
     if (player1.x == 6 && player1.y == 6){
         printf("player1 won!\n");
-        return 0;
+        exit(0);
     }
 
     player2_move:
@@ -109,8 +118,8 @@ int main(){
 
     switch (direction){
         case 8:
-            if ((player2.x - *(ptr + 7 * player2.x + player2.y)) >= 0){
-                player2.x = player2.x - *(ptr + 7 * player2.x + player2.y);
+            if ((player2.x - *(ptr + ROW * player2.x + player2.y)) >= 0){
+                player2.x = player2.x - *(ptr + ROW * player2.x + player2.y);
             }
             else {
                 printf("Invalid direction\n");
@@ -118,8 +127,8 @@ int main(){
             }
             break;
         case 2:
-            if ((player2.x + *(ptr + 7 * player2.x + player2.y)) < 7){
-                player2.x = player2.x + *(ptr + 7 * player2.x + player2.y);
+            if ((player2.x + *(ptr + ROW * player2.x + player2.y)) < ROW){
+                player2.x = player2.x + *(ptr + ROW * player2.x + player2.y);
             }
             else {
                 printf("Invalid direction\n");
@@ -127,8 +136,8 @@ int main(){
             }
             break;
         case 4:
-            if ((player2.y - *(ptr + 7 * player2.x + player2.y)) >= 0){
-                player2.y = player2.y - *(ptr + 7 * player2.x + player2.y);
+            if ((player2.y - *(ptr + COL * player2.x + player2.y)) >= 0){
+                player2.y = player2.y - *(ptr + COL * player2.x + player2.y);
             }
             else {
                 printf("Invalid direction\n");
@@ -136,8 +145,8 @@ int main(){
             }
             break;
         case 6:
-            if ((player2.y + *(ptr + 7 * player2.x + player2.y)) < 7){
-                player2.y = player2.y + *(ptr + 7 * player2.x + player2.y);
+            if ((player2.y + *(ptr + COL * player2.x + player2.y)) < COL){
+                player2.y = player2.y + *(ptr + COL * player2.x + player2.y);
             }
             else {
                 printf("Invalid direction\n");
@@ -154,8 +163,9 @@ int main(){
 
     if (player2.x == 6 && player2.y == 6){
         printf("player2 won!\n");
-        return 0;
+        exit(0);
     }
     
     goto player1_move;
+
 }
