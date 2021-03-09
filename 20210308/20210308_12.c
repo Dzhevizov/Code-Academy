1116c217dd55938b343ae43e4189312942eed10e
Hristo Dzhevizov <76977957+Dzhevizov@users.noreply.github.com>
2021-03-09 23:48:53 +0200

    Create 20210308_12.c

---
+++ 20210308/20210308_12.c
@@ -0,0 +1,32 @@
+/*Изместете позицията си във файла 7 байта след началото, ползвайки fseek.
+int fseek(FILE *pfile, long offset, int original);
+Първият параметър е указател към файла, вторият и третият параметър
+оказват къде точно искаме да отидем във файла.
+Референтни точки с предварително дефинирани имена:
+SEEK_SET- началото на файла
+SEEK_CUR- текуща позиция
+fseek(fp, -10L, SEEK_CUR); връща на зад от текупата 10 bytes.
+SEEK_END- края на файла
+fseek(fp,0L, SEEK_END);
+При текстови файлове, вторият аргумент трябва да е стойността върната от
+ftell()
+За текстови файлови третият аргумент трябва да е SEEK_SET, за бинарни
+файлове и просто число в байтове.*/
+
+#include <stdio.h>
+
+int main(){
+    FILE *fp;
+
+    fp = fopen("file.txt", "w+");
+    fputs("Ivancho and Mariika live in Sofia\n", fp);
+
+    fseek(fp, 20, SEEK_SET);
+    fputs("are moving to Varna\n", fp);
+
+    fseek(fp, -7, SEEK_CUR);
+    fputs("Plovdiv\n", fp);
+    fclose(fp);
+
+    return (0);
+}
