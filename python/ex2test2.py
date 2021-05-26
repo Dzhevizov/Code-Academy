9776c0ed90f511c67177d152e6058f5b26f24c61
Hristo Dzhevizov <76977957+Dzhevizov@users.noreply.github.com>
2021-05-26 12:32:45 +0300

    Update ex2test2.py

--- python/ex2test2.py
+++ python/ex2test2.py
@@ -2,9 +2,7 @@ import unittest
 from unittest.mock import patch 
 from .ex2 import Strings_compare
 
-def demo_getStrings():
-    s1="test"
-    s2="ten"
+def demo_getStrings(s1,s2):
     if len(s1) >= len(s2):
         return s1
     else:
@@ -12,12 +10,12 @@ def demo_getStrings():
 class TestWord(unittest.TestCase):
     @patch('ex2.Strings_compare.get2strings', side_effect = demo_getStrings)
     def test_get2strings(self,get2strings):
-        # print("result of demo_getStrings(test,test10):",demo_getStrings())
-        # self.assertEqual(demo_getStrings(),"test10")
-        # print("result of demo_getStrings(test,text):",demo_getStrings())
-        # self.assertEqual(demo_getStrings(),"test")
-        print("result of demo_getStrings(test,ten):",demo_getStrings())
-        self.assertEqual(demo_getStrings(),"test")
+        print("result of demo_getStrings(test,test10):",demo_getStrings("test","test10"))
+        self.assertEqual(demo_getStrings("test","test10"),"test10")
+        # print("result of demo_getStrings(test,text):",demo_getStrings("test","text"))
+        # self.assertEqual(demo_getStrings("test","text"),"test")
+        # print("result of demo_getStrings(test,ten):",demo_getStrings("test","ten"))
+        # self.assertEqual(demo_getStrings("test","ten"),"test")
 
 if __name__ == "__main__":
     unittest.main()
